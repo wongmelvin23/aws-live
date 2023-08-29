3d11a5a3f0b015fd09ba7da894acc9c076f27200
Melvin Wong <87594022+wongmelvin23@users.noreply.github.com>
2023-08-29 16:32:44 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.cyllxpkkjfhp.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "melvinwongwaihung-employee"
+customregion = "us-east-1"
+
